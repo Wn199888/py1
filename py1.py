5e350e947756aa746a7ac178596f8fc49ed913c1
e <528668936@qq.com>
2021-08-02 20:45:38 +0800

    提交day01作业

---
+++ py1.py
@@ -0,0 +1,42 @@
+
+import xlrd
+book=xlrd.open_workbook("E:/python自动化测试/python课程/day01/任务/12月份衣服销售数据.xlsx")
+sheet1 = book.sheet_by_index(0)
+rows,cols = sheet1.nrows,sheet1.ncols
+for row in range(rows):
+    for col in range(cols):
+        print(sheet1.cell(row,col).value,end='         ')
+    print(' ')
+sumcount=0
+for i in range(1,31):
+    sumcount+=sheet1.cell(i,4).value
+print("销售量：",sumcount)
+sumoney = 0
+for j in range(1,31):
+    sumoney += sheet1.cell(j,2).value * sheet1.cell(j,4).value
+print("总销售额：",sumoney)
+print('平均销售量：',sumcount/30)
+y,n,f,p,t,c = 0,0,0,0,0,0
+for o in range(1,31):
+ if sheet1.cell(o,1).value == '羽绒服':
+     y += sheet1.cell(o,4).value
+ elif sheet1.cell(o,1).value == '牛仔裤':
+     n += sheet1.cell(o,4).value
+ elif sheet1.cell(o,1).value == '风衣':
+     f += sheet1.cell(o,4).value
+ elif sheet1.cell(o,1).value == '皮草':
+     p += sheet1.cell(o,4).value
+ elif sheet1.cell(o,1).value == 'T血':
+     t += sheet1.cell(o,4).value
+ elif sheet1.cell(o,1).value == '衬衫':
+     c += sheet1.cell(o,4).value
+
+
+print('羽绒服销售占比：',253.6*y/sumoney*100,'%')
+print('牛仔裤销售占比：',86.3*n/sumoney*100,'%')
+print('风衣销售占比：',96.8*f/sumoney*100,'%')
+print('皮草销售占比：',135.9*p/sumoney*100,'%')
+print('T血销售占比：',65.8*t/sumoney*100,'%')
+print('衬衫销售占比：',49.3*c/sumoney*100,'%')
+
+
