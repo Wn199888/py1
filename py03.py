b73359d00b1486994ef801cc1fd72b9056996042
Wn199888 <87304043+Wn199888@users.noreply.github.com>
2021-08-05 14:27:35 +0800

    Update py03.py

--- py03.py
+++ py03.py
@@ -1,72 +1,138 @@
-
+#姐就是女王游戏
 import random
-character = ['1.普通','2.稀有英雄','3.传奇英雄']
-print('-------姐就是女王-------')
-i = 0
-while i<3:
-    i+=1
-    choose = int(input('请选择角色：'))
-    a = random.randint(1,200)
-    b = random.randint(1,200)
-    c = random.randint(1,200)
-    rnum = [a,b,c]
-    if choose == 1 :
-        print('普通英雄')
-        init = 10
-        d = random.choice(rnum)
-        e = random.randint(0,1)
-        if e==0 :
-            init +=d
-            if init > 100:
-                print('任务成功！')
-            elif init <= 0:
-                print("任务失败")
-        elif e==1 :
-            print('稀有英雄')
-            init -=d
-            if init > 100:
-                print('任务成功！')
-            elif init <= 0:
-                print("任务失败")
-    elif choose == 2:
-        print('传奇英雄')
-        init1 = 30
-        f = random.choice(rnum)
-        g = random.randint(0, 1)
-        if g == 0:
-            init1 += f
-            if init1 > 100:
-                print('任务成功！')
-            elif init1 <= 0:
-                 print("任务失败")
-        elif g == 1:
-            init1 -= f
-            if init1 > 100:
-                print('任务成功！')
-            elif init1 <= 0:
-                print("任务失败")
-    elif choose == 3:
-           init2 = 10
-           h = random.choice(rnum)
-           init2 += h
-           if init2 > 100:
-               print('任务成功！')
-           else:
-               print("任务失败")
-print('-----------------------')
+list = ['普通','稀有','传奇']
+# print(list[random.randint(0,len(list)-1)])
+name = list[random.randint(0,len(list)-1)]
+list = [1,2]
+num ,num2 = 10,30
+while True:
+    if name == '普通' :
+        print('您获得普通英雄 ')
+        i =0
+        ranumber = []
+        while i<3:
+            i+=1
+            a = random.randint(5,20)
+            ranumber.append(a)
+        print('请选择一个数:',ranumber[0],ranumber[1],ranumber[2])
+        num1 = ranumber[random.randint(0, len(ranumber) - 1)]
+        print(num1)
+        ran1 = list[random.randint(0,len(list)-1)]
+        if ran1 == 1:
+            num = num+num1
+            print(num)
+        else:
+            num =num - num1
+        if num>100:
+            print('任务成功！你的分数为：',num)
+            break
+        elif num <= 0:
+            print('任务失败！你的分数为：',num)
+            break
+    elif name == '稀有' :
+        print('您获得稀有英雄 ')
+        i = 0
+        ranumber1 = []
+        while i < 3:
+            i += 1
+            b = random.randint(5, 20)
+            ranumber1.append(b)
+        print('请选择一个数:', ranumber1[0], ranumber1[1], ranumber1[2])
+        num3 = ranumber1[random.randint(0, len(ranumber1) - 1)]
+        print(num3)
+        ran1 = list[random.randint(0, len(list) - 1)]
+        if ran1 == 1:
+            num2 = num2 + num3
+            print(num2)
+        else:
+            num2 = num2 - num3
+        if num2 > 100:
+            print('任务成功！你的分数为：', num2)
+            break
+        elif num2 <= 0:
+            print('任务失败！你的分数为：', num2)
+            break
+    else:
+        print('您获得传奇英雄 ')
+        i = 0
+        ranumber2 = []
+        while i < 3:
+            i += 1
+            b = random.randint(5, 20)
+            ranumber2.append(b)
+        print('请选择一个数:', ranumber2[0], ranumber2[1], ranumber2[2])
+        num4 = ranumber2[random.randint(0, len(ranumber2) - 1)]
+        print(num4)
+        num2 =num2+num4
+        if num2 > 100:
+            print('任务成功！，你的分数为：',num2)
+            break
 
-'''
-1.随机点名
-2.随机生成棒棒糖
 
-容器类型：
-        int, str,double,boolean
-        元组（tuple）：（）--不可修改
-        列表(list)：[]
-            append()添加数据，元素；pop():删除从后往前删除 角标最后一个为-1
-        字典：{‘’：‘’} 键值对
-              键   值   通过键获取值
-'''
+#
+# import random
+# print('-------姐就是女王-------')
+# character = ['1.普通英雄','2.稀有英雄','3.传奇英雄']
+# init = [10,30]
+# ran = [random.randint(5,20),random.randint(5,20),random.randint(5,20)]
+# cha = random.choice(character)
+# list = [1,2]
+# score = init[0]
+# while 1:
+#     if cha == character[0]:
+#         print("您获得的英雄是：", cha)
+#         print('请选择一个数：',ran[0],ran[1],ran[2])
+#         sele = int(input())
+#         if sele == ran[0]:
+#             a = list[random.randint(0,len(list)-1)]
+#             if a == 1:
+#                 score += sele
+#             else :
+#                 score += sele
+#             if score>100:
+#                 print('任务成功，你的分数为：',score)
+#                 break
+#             elif score<=0:
+#                 print('任务失败，你的分数为：',score)
+#                 break
+#         if sele == ran[1]:
+#             a = list[random.randint(0,len(list)-1)]
+#             if a == 1:
+#                 score += sele
+#             else :
+#                 score += sele
+#             if score>100:
+#                 print('任务成功，你的分数为：',score)
+#                 break
+#             elif score<=0:
+#                 print('任务失败，你的分数为：',score)
+#                 break
+#         if sele == ran[2]:
+#             a = list[random.randint(0,len(list)-1)]
+#             if a == 1:
+#                 score += sele
+#             else :
+#                 score += sele
+#             if score>100:
+#                 print('任务成功，你的分数为：',score)
+#                 break
+#             elif score<=0:
+#                 print('任务失败，你的分数为：',score)
+#                 break
+
+
+# '''
+# 1.随机点名
+# 2.随机生成棒棒糖
+#
+# 容器类型：
+#         int, str,double,boolean
+#         元组（tuple）：（）--不可修改
+#         列表(list)：[]
+#             append()添加数据，元素；pop():删除从后往前删除 角标最后一个为-1
+#         字典：{‘’：‘’} 键值对
+#               键   值   通过键获取值
+# '''
 # name = (1,2,3,4,5)
 # print(type(name))
 
